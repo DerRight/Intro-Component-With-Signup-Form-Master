ac79d7a9088c5f5a190bd62a408c393b6760ed7c
DerRight <zxc123456789123456@gmail.com>
2024-10-31 22:36:50 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function validInformation() {
+    const firstName = document.getElementById('first-name');
+    const lassName = document.getElementById('last-name');
+    const emailAddress = document.getElementById('email-address');
+    const paddword = document.getElementById('password');
+
+    if (!)
+}
